14cd86205a895415ee1ec137d5228b160f2fef13
Afeef <afeef9706@gmail.com>
2024-01-18 02:50:29 +0800

    Feature/next-theme setup
    feat: installed next-themes
    
    chore: moved root files
    
    feat: added provider component

---
+++ src/app/(root)/Providers.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ThemeProvider } from 'next-themes';
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return <ThemeProvider>{children}</ThemeProvider>;
+};
+
+export default Providers;

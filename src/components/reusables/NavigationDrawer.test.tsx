824c85e3a47790c08798be87dbcc1c693273e07c
Afeef <afeef9706@gmail.com>
2024-01-20 10:19:09 +0800

    Feature/Mobile navigation drawer
    feat: added drawer from shadcn
    
    chore: NavigationDrawer unit tests
    
    feat: updated color
    
    feat: NavigationDrawer component

---
+++ src/components/reusables/NavigationDrawer.test.tsx
@@ -0,0 +1,66 @@
+import { render, screen } from '@testing-library/react';
+import { ILinks } from '@/lib/types';
+import NavigationDrawer from './NavigationDrawer';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
+
+describe('NavigationDrawer component', () => {
+  window.scrollTo = jest.fn();
+  it('should render after clicking the button', async () => {
+    const user = userEvent.setup();
+    render(<NavigationDrawer links={[]} />);
+
+    const openNavigationDrawer = screen.getByRole('button', {
+      name: /^Open navigation drawer$/i,
+    });
+
+    await act(async () => {
+      await user.click(openNavigationDrawer);
+    });
+    const navigationDrawer = screen.getByTestId('navigation-drawer');
+
+    expect(navigationDrawer).toBeInTheDocument();
+  });
+
+  it('should have a button to open the drawer', () => {
+    render(<NavigationDrawer links={[]} />);
+
+    const button = screen.getByRole('button', {
+      name: /^Open navigation drawer$/i,
+    });
+
+    expect(button).toBeInTheDocument();
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+});
+
+describe('NavigationDrawer links', () => {
+  it('should render links', async () => {
+    const links: ILinks[] = [
+      {
+        label: 'Test 1',
+        href: '/',
+      },
+      {
+        label: 'Test 2',
+        href: '/test-2',
+      },
+    ];
+    const user = userEvent.setup();
+    render(<NavigationDrawer links={links} />);
+
+    const openNavigationDrawer = screen.getByRole('button', {
+      name: /^Open navigation drawer$/i,
+    });
+
+    await act(async () => {
+      await user.click(openNavigationDrawer);
+    });
+    const linksElement = screen.getAllByRole('link');
+
+    expect(linksElement).toHaveLength(2);
+  });
+});

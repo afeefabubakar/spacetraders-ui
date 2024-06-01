489585d509fc1231adac2a099b99839b5811e816
Afeef <contact@afeef.dev>
2024-06-01 21:44:12 +0800

    Feature/added next themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,25 @@
-import type { Metadata } from "next";
-import { Kode_Mono } from 'next/font/google';
 import './globals.css';
+import { Kode_Mono } from 'next/font/google';
+import type { Metadata } from 'next';
+import Providers from '@/components/providers/Providers';
 
 const kodeMono = Kode_Mono({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Spacetraders UI',
-  description:
-    'An unofficial UI made by Afeef for Spacetraders, an API-based game.',
+    title: 'Spacetraders UI',
+    description: 'An unofficial UI made by Afeef for Spacetraders, an API-based game.',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang='en'>
-      <body className={kodeMono.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={kodeMono.className}>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
 }
---
+++ src/components/providers/Providers.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ThemeProvider from './ThemeProvider';
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <ThemeProvider
+            attribute='class'
+            defaultTheme='dark'
+            disableTransitionOnChange
+        >
+            {children}
+        </ThemeProvider>
+    );
+};
+
+export default Providers;
